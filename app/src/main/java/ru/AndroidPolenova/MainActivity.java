d487f98fb2abbc51480dafbe0562073fbeafd085
Marina <M.V.Polenova@users.noreply.github.com>
2019-10-30 12:55:19 +0100

    homework1.2.1 edit

--- app/src/main/java/ru/AndroidPolenova/MainActivity.java
+++ app/src/main/java/ru/AndroidPolenova/MainActivity.java
@@ -13,33 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        final EditText editName = findViewById(R.id.editNameUser);
+        final EditText editPassword = findViewById(R.id.editPasswordUser);
+        final EditText editMail = findViewById(R.id.editMailUser);
+        final TextView result = findViewById(R.id.textResult);
         Button btnClear = (Button) findViewById(R.id.buttonClear);
         Button btnOK = (Button) findViewById(R.id.buttonOK);
         btnOK.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View v){
-                EditText editName = findViewById(R.id.editNameUser);
-                EditText editPassword = findViewById(R.id.editPasswordUser);
-                EditText editMail = findViewById(R.id.editMailUser);
-                String name = editName.getText().toString();
-                String password = editPassword.getText().toString();
-                String mail = editMail.getText().toString();
-                TextView result = findViewById(R.id.textResult);
-                result.setText("Подписка на рассылку оформлена для пользователя " + name + "  на электронный адрес " + mail);
-
+                String info = String.format(getString(R.string.info), editName.getText(), editMail.getText());
+                result.setText(info);
             }
         });
         btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText editName = findViewById(R.id.editNameUser);
-                EditText editPassword = findViewById(R.id.editPasswordUser);
-                EditText editMail = findViewById(R.id.editMailUser);
-                TextView result = findViewById(R.id.textResult);
                 editName.getText().clear();
                 editPassword.getText().clear();
                 editMail.getText().clear();
-                result.setText("нет данных");
+                result.setText(R.string.ClearTxt);
             }
         });
     }
